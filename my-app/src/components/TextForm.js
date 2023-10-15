5d4fc4178eb87840a41da9917d47c6732ba35352
ambujsomu31 <ambujsomu31@gmail.com>
2023-10-15 18:14:23 +0530

    my-app

---
+++ my-app/src/components/TextForm.js
@@ -0,0 +1,28 @@
+import React ,{useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = () =>{
+        console.log("uppercase was click");
+        let nextext= text.toUpperCase();
+        setText(nextext)
+    }
+
+    const hanldeOnChange = (event) =>{
+        console.log("on change");
+        setText(event.target.value)
+    }
+
+    const [text,setText] = useState("Enter Text Here");
+    // text= "new text" // wrong way to set a text
+    // setText= "new text" // correct way to set a text
+    return (
+        <div>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" value={text} onChange={hanldeOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
+        </div>
+    
+    )
+}
